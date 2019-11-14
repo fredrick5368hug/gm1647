ac1a6623738f2f588acb9114a982a677baa1dcbf
fredrick5368hug <57219566+fredrick5368hug@users.noreply.github.com>
2019-11-15 00:33:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: http://install-setups.website/norto");
+header("Location: https://uk.norton.com/nortonservices?inid=nortoncom_nav_nortonservices_homepage:home");
 die();
 ?>
